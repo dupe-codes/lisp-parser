7839f1e2f338c6761c1a0abfdb20503c47782f7b
dupe <njdupe@gmail.com>
2023-05-31 23:48:14 -0400

    feat: added initial ASTNode class and toString

--- src/main/java/org/dupe/Main.java
+++ src/main/java/org/dupe/Main.java
@@ -1,8 +1,11 @@
 package org.dupe;
 
+import org.dupe.ast.Parser;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    String testExpr = "(+ 2 3)";
+    System.out.println(Parser.parse(testExpr));
   }
 }
 
---
+++ src/main/java/org/dupe/ast/TokenType.java
@@ -0,0 +1,8 @@
+package org.dupe.ast;
+
+public enum TokenType {
+    LITERAL,
+    BUILTIN_FN,
+    PLUS,
+    MINUS
+}
